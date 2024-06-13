d99934a41d66cc3965a6ed29989fa9889c798bb8
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2024-06-13 08:39:44 +0200

    Update const.py

--- src/const.py
+++ src/const.py
@@ -3,6 +3,6 @@ LOG_FORMAT = '[%(asctime)s] [%(levelname)s] %(message)s'
 MAX_LOGSIZE = 1024 * 1024  # 1Mb
 LOG_COUNT = 3
 
-IP_LIST = ['89.79.244.70', '127.0.0.1']
+IP_LIST = ['89.79.244.70', '195.150.104.138', '127.0.0.1']
 
 STATIC_PATH = './static'
